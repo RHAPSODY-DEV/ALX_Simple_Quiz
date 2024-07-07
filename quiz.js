63dbd9f8e077ce96c6f6c1fa3d31f60bc39ef123
RHAPSODY <rhapsodyquame@gmail.com>
2024-07-07 19:55:35 +0100

    added new files

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer () {
+    let correctAnswer = "4";
+    document.querySelector('input[name="quiz"]:checked').value = "userAnswer";
+
+    if (userAnswer ===  correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+
+    }
+    else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+
+    }
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
