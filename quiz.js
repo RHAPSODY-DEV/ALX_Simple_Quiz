2f8bb24140025ff3cbd7740825791c922a6fc873
RHAPSODY <rhapsodyquame@gmail.com>
2024-07-10 16:41:17 +0100

    mades some changes

--- quiz.js
+++ quiz.js
@@ -15,3 +15,14 @@ function checkAnswer() {
 
 // Add event listener to the submit button
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
+
+// The line document.querySelector('input[name="quiz"]:checked').value; is used to retrieve the value of the selected radio button from a group of radio buttons with the name "quiz".
+
+// Let's break it down:
+
+// document.querySelector: This is a method that returns the first element within the document that matches the specified selector, or group of selectors.
+// input[name="quiz"]: This is the selector that targets <input> elements with a name attribute equal to "quiz". This is likely a group of radio buttons.
+// :checked: This is a pseudo-class that targets the radio button that is currently checked.
+// .value: This property returns the value of the selected radio button.
+// So, when you combine all of these, document.querySelector('input[name="quiz"]:checked').value; returns the value of the currently selected radio button from the group of radio buttons with the name "quiz".
